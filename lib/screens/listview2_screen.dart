0970346813e30b0b078761ae9e3bcac6e76f3723
marwilc <marwilcampos@gmail.com>
2023-01-07 13:54:20 -0400

    :sparkles: add function on tap listview

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -12,12 +12,23 @@ class ListView2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('List view2')),
+        appBar: AppBar(
+          title: const Text('List view2'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
+        ),
         body: ListView.separated(
           itemCount: options.length,
           itemBuilder: ((_, index) => ListTile(
                 title: Text(options[index]),
-                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  color: Colors.indigo,
+                ),
+                onTap: () {
+                  final game = options[index];
+                  print(game);
+                },
               )),
           separatorBuilder: ((_, __) => const Divider()),
         ));
