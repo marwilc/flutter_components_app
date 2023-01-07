af328537d84df5360419637b0076ac9f911d9490
marwilc <marwilcampos@gmail.com>
2023-01-07 14:39:25 -0400

    :sparkles: addes home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: ((context, index) => ListTile(
+                  leading: const Icon(Icons.access_time_outlined),
+                  title: const Text('Nombre de la ruta'),
+                  onTap: () {
+                    
+                  },
+                )),
+            separatorBuilder: ((_, __) => const Divider()),
+            itemCount: 10));
   }
 }
