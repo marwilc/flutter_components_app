9fb14d75f6775a096c5b71957deab250081f87cb
marwilc <marwilcampos@gmail.com>
2023-01-25 23:41:48 -0400

    :sparkles: added button style

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child:  Text(
+              'Mostrar alerta',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.indigo,
+          child: const Icon(Icons.close),
+          onPressed: () {}),
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,25 +3,26 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color primary = Colors.indigo;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    // Color primario
-    primaryColor: primary,
+      // Color primario
+      primaryColor: primary,
 
-    // appbar theme
-    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
-
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(primary: primary)
-    )
-
-  );
+      // appbar theme
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(backgroundColor: primary),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.indigo,
+              shape: const StadiumBorder(),
+              elevation: 0)));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    // Color primario
-    primaryColor: primary,
+      // Color primario
+      primaryColor: primary,
 
-    // appbar theme
-    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
-
-    scaffoldBackgroundColor: Colors.black
-  );
+      // appbar theme
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      scaffoldBackgroundColor: Colors.black);
 }
